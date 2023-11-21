ccb5af1b5dd5af90405127c5bda1c93642ff5442
Muhammad Iqbal <muhammadiqbalid83@gmail.com>
2023-11-22 06:16:51 +0700

    feat: add a landing page

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,3 +1,19 @@
+import { FeaturedProducts, Hero } from '../components'
+import { customFetch } from '../utils'
+
+const url = '/products?featured=true'
+
+export const loader = async () => {
+  const response = await customFetch(url)
+  const products = response.data.data
+  return { products }
+}
+
 export default function Landing() {
-  return <div>Landing</div>
+  return (
+    <>
+      <Hero />
+      <FeaturedProducts />
+    </>
+  )
 }
