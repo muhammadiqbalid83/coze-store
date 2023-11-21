9071c78f3da3b91eed3caad1c680dc2a302bbb5a
Muhammad Iqbal <muhammadiqbalid83@gmail.com>
2023-11-21 08:47:31 +0700

    feat: remove nav

--- src/pages/HomeLayout.tsx
+++ src/pages/HomeLayout.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router-dom'
+import { Header, Navbar } from '../components'
 export default function HomeLayout() {
   return (
     <>
-      <nav>
-        <span className="text-4xl text-primary">Coze</span>
-      </nav>
-      <Outlet />
+      <Header />
+      <Navbar />
+      <section className="align-element py-20">
+        <Outlet />
+      </section>
     </>
   )
 }
