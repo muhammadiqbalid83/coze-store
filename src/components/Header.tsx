34b0b92b4a4d4591dc15f9b79da7158172947ba0
Muhammad Iqbal <muhammadiqbalid83@gmail.com>
2023-11-23 15:13:08 +0700

    style: remove class

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,10 +2,8 @@ import { Link } from 'react-router-dom'
 
 export default function Header() {
   return (
-    <header className="bg-neutral py-2 text-neutral-content">
+    <header className="bg-neutral py-2 ">
       <div className="align-element flex justify-center sm:justify-end">
-        {/* users */}
-        {/* links */}
         <div className="flex gap-x-6 justify-center items-center">
           <Link to="/login" className="link link-hover text-xs sm:text-sm">
             Sign in / guest
