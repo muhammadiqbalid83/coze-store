cdb50de284641c22914858b0623ac5f1aab06dd4
Muhammad Iqbal <muhammadiqbalid83@gmail.com>
2023-11-23 11:38:40 +0700

    feat: add redux toolkit & toastfy setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+
+import 'react-toastify/dist/ReactToastify.css'
 import './index.css'
+import { ToastContainer } from 'react-toastify'
+import { store } from './store.ts'
+import { Provider } from 'react-redux'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+    <ToastContainer position="top-center" />
+  </Provider>
 )
