5bea234f28f5964540e04c036b492d38e65654ae
Muhammad Iqbal <muhammadiqbalid83@gmail.com>
2023-11-25 10:07:00 +0700

    refactor: change js to ts

--- src/store.ts
+++ src/store.ts
@@ -1,9 +1,15 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
 
 import cartReducer from './features/cart/cartSlice'
+import userReducer from './features/user/userSlice'
+
+const rootReducer = combineReducers({
+  cartState: cartReducer,
+  userState: userReducer,
+})
+
+export type RootState = ReturnType<typeof rootReducer>
 
 export const store = configureStore({
-  reducer: {
-    cartState: cartReducer,
-  },
+  reducer: rootReducer,
 })
